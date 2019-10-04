ef9f8edd2d1ef139ab8ffbf9213f958e79b3d7d7
Uorriuart <Uorriuart@yandex.ru>
2019-10-04 23:12:52 +0300

    Edit class Main.java a bit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Folder find text");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
