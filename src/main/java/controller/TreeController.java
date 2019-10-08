d88695e7091df638306d62227edc21bc495e68f1
Uorriuart <Uorriuart@yandex.ru>
2019-10-08 23:53:38 +0300

    Add new method for getting required file

--- src/main/java/controller/TreeController.java
+++ src/main/java/controller/TreeController.java
@@ -1,19 +1,24 @@
 package main.java.controller;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import main.java.model.InputValidator;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 class TreeController {
     private TreeItem<String> root;
+    private Map<TreeItem<String>, File> files = new HashMap<>();
 
     TreeController(InputValidator inputValidator, TreeView<String> fileTree) {
         root = new TreeItem<>(inputValidator.getFolder().getName());
         fileTree.setRoot(root);
     }
+
 
     void findFilesInFolder(ArrayList<File> files) {
         //files.forEach(this::addFile);
@@ -26,11 +31,22 @@ class TreeController {
     private void findFilesInFolderWithRecursion(TreeItem<String> current, File file) {
         TreeItem<String> parent = new TreeItem<>(file.getParentFile().getName());
         parent.getChildren().add(current);
+        files.put(current, file);
         if (root.toString().equals(parent.toString())) {
             root.getChildren().add(current);
         } else {
             findFilesInFolderWithRecursion(parent, file.getParentFile());
         }
+        for (Map.Entry entry: files.entrySet()) {
+            System.out.println(entry);
+
+        }
     }
+
+    File getFile(TreeItem<String> item) {
+        return files.get(item);
+
+    }
+
 
 }
